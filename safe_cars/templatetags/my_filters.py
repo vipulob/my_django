c76768e87edf84cd9eb01a9dd5a330540b885733
Vipul Borikar <vipulcell@gmail.com>
2024-05-01 17:49:27 +0530

    * Added few entires of the new car ncap results

--- safe_cars/templatetags/my_filters.py
+++ safe_cars/templatetags/my_filters.py
@@ -4,8 +4,13 @@ register = Library()
 
 @register.filter(name='times') 
 def times(number):
+    
+    if isinstance(number, str):
+        return None
     return range(number)
 
 @register.filter(name='timesminus') 
 def timesminus(number):
+    if isinstance(number, str):
+        return None
     return range(5-number)
